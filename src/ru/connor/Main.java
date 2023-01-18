fa2938e9fcfb5998f020d45f8ab9e2bd7158761e
John Connor <90540757+Xmoon1@users.noreply.github.com>
2023-01-18 19:04:33 +0700

    02

--- src/ru/connor/Main.java
+++ src/ru/connor/Main.java
@@ -1,7 +1,43 @@
 package ru.connor;
 
+
+import java.util.Scanner;
+
+/**
+ * @author John
+ */
 public class Main {
+    public String a;
+    private int b;
+
     public static void main(String[] args) {
-        System.out.println("ehhllo");
+        //TODO
+
+        // Модификаторы доступа public private protected default
+//        int a = 6;
+//        int b = 4;
+
+        // Arithmetic operations: +, -, *, /, %
+//        int c = a + b; // 10
+//        int c = a - b; // 2
+//        int c = a * b; // 24
+//        int c = a / b; // 1
+//        int c = a % b; // 2
+//        String b = "Hello";
+//        Double d = 3.34;
+
+        // Типы данных: String, int, double, float, boolean
+
+//        String str1 =  "Hello";
+//        String str2 = " world";
+//        String s3 = str1 + str2;
+//        System.out.println(s3);
+
+//        Scanner scanner = new Scanner(System.in);
+//        int a = scanner.nextInt();
+//        int b = scanner.nextInt();
+//        String myName = scanner.next();
+//
+//        System.out.println("Hello " + myName);
     }
 }
