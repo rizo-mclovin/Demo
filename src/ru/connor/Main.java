b06fcbad5d6b8c1fb8e7671113453a0ac804f31c
John Connor <90540757+Xmoon1@users.noreply.github.com>
2023-01-19 16:56:06 +0700

    03

--- src/ru/connor/Main.java
+++ src/ru/connor/Main.java
@@ -1,43 +1,30 @@
 package ru.connor;
 
-
-import java.util.Scanner;
+import java.util.Locale;
 
 /**
  * @author John
  */
 public class Main {
-    public String a;
-    private int b;
-
     public static void main(String[] args) {
-        //TODO
+        // While
+        int number = 5;
+        int number2 = 10;
+        String str = "BTC_USDT";
+        String str2 = "Hello";
 
-        // Модификаторы доступа public private protected default
-//        int a = 6;
-//        int b = 4;
-
-        // Arithmetic operations: +, -, *, /, %
-//        int c = a + b; // 10
-//        int c = a - b; // 2
-//        int c = a * b; // 24
-//        int c = a / b; // 1
-//        int c = a % b; // 2
-//        String b = "Hello";
-//        Double d = 3.34;
-
-        // Типы данных: String, int, double, float, boolean
-
-//        String str1 =  "Hello";
-//        String str2 = " world";
-//        String s3 = str1 + str2;
-//        System.out.println(s3);
-
-//        Scanner scanner = new Scanner(System.in);
-//        int a = scanner.nextInt();
-//        int b = scanner.nextInt();
-//        String myName = scanner.next();
+        System.out.println(str.toUpperCase());
+        System.out.println(str.toLowerCase());
+        System.out.println(!str.contains("_USDT"));
+//        // 1
+//        while (number < 7){
+//            System.out.println("Hello");
+//        }
 //
-//        System.out.println("Hello " + myName);
+//        // 2
+//        do{
+//            number += 1;
+//            System.out.println("Hello");
+//        }while (number < 7);
     }
 }
