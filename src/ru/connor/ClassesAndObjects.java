21c13c8f3efdc69142ed0bb8e2dbfaeb72a8b693
John Connor <90540757+Xmoon1@users.noreply.github.com>
2023-02-28 22:07:35 +0700

    methods

---
+++ src/ru/connor/ClassesAndObjects.java
@@ -0,0 +1,17 @@
+package ru.connor;
+
+public class ClassesAndObjects{
+    public static void main(String[] args) {
+        Person person = new Person();
+        
+    }
+}
+
+class Person{
+    /**
+     * 1. Поля (Данные)
+     * 2. Методы (Действия)
+     */
+    String name;
+    int age;
+}
