52f4a652e3f44cb6d62ef4b6a1fba53f82488931
John Connor <90540757+Xmoon1@users.noreply.github.com>
2023-02-28 22:09:51 +0700

    methods

--- src/ru/connor/ClassesAndObjects.java
+++ src/ru/connor/ClassesAndObjects.java
@@ -1,9 +1,15 @@
 package ru.connor;
 
+import java.time.Year;
+
 public class ClassesAndObjects{
     public static void main(String[] args) {
-        Person person = new Person();
-        
+        Person person = new Person("John", 2006);
+
+        System.out.println(person.sayHello());
+        System.out.println(person.calculateAge(1983));
+        System.out.println(person);
+
     }
 }
 
@@ -13,5 +19,36 @@ class Person{
      * 2. Методы (Действия)
      */
     String name;
-    int age;
+    int birthYear;
+
+   // <модификатор доступа> <Название класса>(<тип> <название переменной>, int age){
+    //    this.name = name;
+    // }
+    public Person(String name, int birthYear) {
+        this.name = name;
+        this.birthYear = birthYear;
+    }
+    public Person() {}
+
+    // <модификатор доступа> <тип возврощаемого значения> <Название метода>(<тип> <название переменной>, int age){
+    //        <действие>
+    //        return <результат>;
+    // }
+
+    public String sayHello(){
+        return "Hello " + name;
+    }
+
+    public int calculateAge(int birthYear){
+        int age = Integer.parseInt(Year.now().toString()) - birthYear;
+        return age;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", birthYear=" + birthYear +
+                '}';
+    }
 }
