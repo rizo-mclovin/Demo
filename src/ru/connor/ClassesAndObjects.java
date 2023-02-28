70e48dacc959106c48f00ee519bee7937762e2c5
John Connor <90540757+Xmoon1@users.noreply.github.com>
2023-02-28 23:10:49 +0700

    classes_methods

--- src/ru/connor/ClassesAndObjects.java
+++ src/ru/connor/ClassesAndObjects.java
@@ -3,52 +3,54 @@ package ru.connor;
 import java.time.Year;
 
 public class ClassesAndObjects{
-    public static void main(String[] args) {
-        Person person = new Person("John", 2006);
 
-        System.out.println(person.sayHello());
-        System.out.println(person.calculateAge(1983));
-        System.out.println(person);
+    public static void main(String[] args) {
+        Person person = new Person();
+        person.name = "John";
+        person.birthYear = 2006;
+
+        person.sayHello();
+
+        Person person2 = new Person();
+        person2.name = "Alice";
+        person2.birthYear = 2003;
+
+        person2.sayHello();
+
 
     }
 }
 
-class Person{
+class Person {
     /**
      * 1. Поля (Данные)
      * 2. Методы (Действия)
      */
-    String name;
-    int birthYear;
 
-   // <модификатор доступа> <Название класса>(<тип> <название переменной>, int age){
-    //    this.name = name;
-    // }
-    public Person(String name, int birthYear) {
-        this.name = name;
-        this.birthYear = birthYear;
-    }
-    public Person() {}
+    // public, protected, private, default
+    public String name;
+    public int birthYear;
 
-    // <модификатор доступа> <тип возврощаемого значения> <Название метода>(<тип> <название переменной>, int age){
-    //        <действие>
-    //        return <результат>;
+    // <модификатор доступа> void <название метода>(){
+    //      <Действия>
     // }
 
-    public String sayHello(){
-        return "Hello " + name;
+    public void sayHello(){
+        System.out.println("My name is " + name + ". Me " + calculateAge() + " y.o.");
     }
 
-    public int calculateAge(int birthYear){
+    // <модификатор доступа> <тип> <название метода>(){
+    //      <Действия>
+    //      return <результат>
+    // }
+
+//    public void calculateAge(){
+//        int age = 2023 - birthYear;
+//        System.out.println(age);
+//    }
+
+    public int calculateAge(){
         int age = Integer.parseInt(Year.now().toString()) - birthYear;
         return age;
-    }
-
-    @Override
-    public String toString() {
-        return "Person{" +
-                "name='" + name + '\'' +
-                ", birthYear=" + birthYear +
-                '}';
     }
 }
