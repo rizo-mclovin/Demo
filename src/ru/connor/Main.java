8a851aafd2b137a44742eba9738fdf839477c292
John Connor <90540757+Xmoon1@users.noreply.github.com>
2023-03-13 22:23:38 +0700

    cycle_for

--- src/ru/connor/Main.java
+++ src/ru/connor/Main.java
@@ -1,36 +1,70 @@
 package ru.connor;
 
-import java.sql.Date;
-import java.time.Instant;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-//        int a = scanner.nextInt();
-//        int b = scanner.nextInt();
-        // while(<условие>){
-        //     <инструкция1>
-        // }
+        // for (<создаем переменную i>; <условие, до каких пор будет выполняться программа>; <операция>){
+        //      <инструкция>
+        //}
 
-
-        int i = 0;
-
-//        while (i < 10){
+//        int number = scanner.nextInt();
 //
-//            i = i + 1;
-//            System.out.println(i);
 //
+
+        for (int i=0; i < 10; i++) {
+            if ((i % 2 == 0)){
+                continue;
+            }
+            System.out.println("Hello world. " + i);
+        }
+
+
+//        if (number == 12 || number == 1 || number == 2){
+//            System.out.println("A");
+//        } else if (number == 3 || number == 4 || number == 5) {
+//            System.out.println("B");
+//        }
+//        else if (number == 6 || number == 7 || number == 8) {
+//            System.out.println("C");
+//
+//        } else if (number == 9 || number == 10 || number == 11) {
+//            System.out.println("D");
+//
+//        } else {
+//            System.out.println("ERROR");
 //        }
 
-        do {
-            i = i + 1;
-            System.out.println(i);
 
-        }while (i < 10);
+//         switch(number){
+//            case 12, 1, 2:
+//                System.out.println("A");
+//                break;
+//            case 3, 4 ,5:
+//                 System.out.println("B");
+//                 break;
+//            case 6, 7, 8:
+//                 System.out.println("C");
+//                 break;
+//            case 9, 10, 11:
+//                 System.out.println("D");
+//                 break;
+//             default:
+//                 System.out.println("ERROR");
+//         }
 
 
-        System.out.println(Date.from(Instant.now()));
+//        while (true){
+//            number ++;
+//            if (number >= 10){
+//                break;
+//            }
+//            System.out.println(number);
+//        }
+
     }
 }
+
+
---
+++ src/ru/connor/Test.java
@@ -0,0 +1,7 @@
+package ru.connor;
+
+public class Test {
+    public static void main(String[] args) {
+
+    }
+}
