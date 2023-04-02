5b7ad506bcbb8b5a7fac1097f5102275b8b66b80
John Connor <90540757+Xmoon1@users.noreply.github.com>
2023-04-02 22:21:31 +0700

    work-with-files

--- src/ru/connor/Main.java
+++ src/ru/connor/Main.java
@@ -1,52 +1,17 @@
 package ru.connor;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
 
 public class Main {
-    public static void main(String[] args) {
-//        List<Animals> animalsList = new ArrayList<>();
-//        animalsList.add(new Animals(1));
-//        animalsList.add(new Animals(2));
-//        animalsList.add(new Animals(3));
-//        test(animalsList);
-//
-//        List<Dog> dogList = new ArrayList<>();
-//        dogList.add(new Dog());
-//        dogList.add(new Dog());
-//        test(dogList);
+    public static void main(String[] args) throws FileNotFoundException {
+        String separator = File.separator;
+        String path = separator+"Users"+separator+"dora"+separator+"Desktop"+separator+"file.txt";
+        File file = new File(path);
 
-
-        Scanner scanner = new Scanner(System.in);
-
-        int num = scanner.nextInt();
-
-        try{
-
-
-            System.out.println(10/num);
-        }catch (Exception e){
-            System.out.println(e.getMessage());
-        }
-//        try {
-////            int a = scanner.nextInt();
-////            System.out.println(10/a);
-//
-//            int[] numbers = new int[2];
-//            numbers[5] = 32;
-//            for (int i : numbers){
-//                System.out.println(i);
-//            }
-//        }catch (Exception e){
-//            System.out.println(e.getMessage());
-//        }
-    }
-
-    public static void test(List<? extends Animals> list) {
-        for (Animals animals : list
-        ) {
-            animals.sleep();
-        }
+        PrintWriter pw = new PrintWriter(file);
+        pw.println("Hello");
+        pw.println("world!");
+        pw.close();
     }
 }
