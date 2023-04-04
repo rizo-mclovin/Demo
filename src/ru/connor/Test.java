69153f674ab647269811e65474834f7268201564
John Connor <90540757+Xmoon1@users.noreply.github.com>
2023-04-04 21:00:10 +0700

    lesson03.04

--- src/ru/connor/Test.java
+++ src/ru/connor/Test.java
@@ -1,41 +1,15 @@
 package ru.connor;
 
+import java.util.Locale;
 import java.util.Scanner;
 
 public class Test {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args)
+    {
+        Scanner in = new Scanner(System.in);
+        in.useLocale(Locale.ROOT);
 
-        // Задача 1
-//        int a = scanner.nextInt();
-//        int b = scanner.nextInt();
-//        int res = 0;
-//
-//        for (int i = 0; i < b; i++) {
-//            res  += a;
-//        }
-//        System.out.println(res);
-
-        // Задача 2
-//        int a = scanner.nextInt();
-//        int res = 0;
-//
-//        for (int i = 0; i <= a; i++) {
-//            res += i;
-//        }
-//        System.out.println(res);
-        // Задача 3
-
-        int number = scanner.nextInt();
-        int b = 0;
-
-//        for (int i = 0; i <= number; i++) {
-//            System.out.println(i);
-//        }
-
-        while (b < number){
-            b++;
-            System.out.println(b*2);
-        }
+        double x = in.nextDouble();
+        System.out.println(x>=3 && x<=8);
     }
 }
