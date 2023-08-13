4eac3ee8ebf53c8a7a81f06922c1f3a5e9249f6e
John Connor <johnconnor04@mail.ru>
2023-08-13 16:56:17 +0700

    first ilya commit

--- src/ru/connor/group/Animals.java
+++
@@ -1,7 +0,0 @@
-package ru.connor.group;
-
-public class Animals {
-    public void eat(){
-        System.out.println("Animal is eating..");
-    }
-}
--- src/ru/connor/group/Cat.java
+++
@@ -1,8 +0,0 @@
-package ru.connor.group;
-
-public class Cat extends Animals {
-    @Override
-    public void eat() {
-        System.out.println("Cat is eating");
-    }
-}
