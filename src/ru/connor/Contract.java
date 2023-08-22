2d6d155bbb7de5c4fde4bb29afd9e6d323878009
John Connor <johnconnor04@mail.ru>
2023-08-22 22:05:50 +0700

    map interface

--- src/ru/connor/Contract.java
+++
@@ -1,6 +0,0 @@
-package ru.connor;
-
-public class Contract {
-
-
-}
--- src/ru/connor/Main.java
+++ src/ru/connor/Main.java
@@ -1,7 +1,59 @@
 package ru.connor;
 
+
+import java.util.*;
+
+/**
+ * <h1>Map</h1>
+ */
 public class Main {
     public static void main(String[] args) {
+        Map<Integer,String> hashMap = new HashMap<>();  // никакого порядка
+        Map<Integer,String> linkedHashMap = new LinkedHashMap<>(); // порядок по добавления
+        Map<Integer,String> treeMap = new TreeMap<>();  //
+
+        test(hashMap);
+        System.out.println("########################");
+        test(linkedHashMap);
+        System.out.println("########################");
+        test(treeMap);
+
+        // aa < ab
+
+//        map.put(1, "one");
+//        map.put(2, "two");
+//        map.put(3, "three");
+//
+//        map.put(3, "hello");
+//
+//        String a = "hello";
+//
+//
+//        for (Map.Entry<Integer, String> entry: map.entrySet()) {
+//            System.out.printf("%d : %s", entry.getKey(), entry.getValue());
+//            System.out.println();
+//        }
+    }
+
+    public static void test(Map<Integer, String> map){
+        map.put(8, "eight");
+        map.put(1,"one");
+        map.put(3, "three");
+        map.put(6, "three");
+        map.put(5, "two");
+        map.put(4,"one");
+        map.put(2, "two");
+        map.put(9, "nine");
+        map.put(10, "ten");
+        map.put(7, "three");
+
+
+        for (Map.Entry<Integer, String> entry: map.entrySet()) {
+            System.out.printf("%d : %s", entry.getKey(), entry.getValue());
+            System.out.println();
+        }
 
     }
+
+
 }
