1be9da9ba688efe2ac8fc825e899a6fcce268a79
John Connor <johnconnor04@mail.ru>
2023-08-22 22:56:30 +0700

    set interface

--- src/ru/connor/Main.java
+++ src/ru/connor/Main.java
@@ -8,52 +8,36 @@ import java.util.*;
  */
 public class Main {
     public static void main(String[] args) {
-        Map<Integer,String> hashMap = new HashMap<>();  // никакого порядка
-        Map<Integer,String> linkedHashMap = new LinkedHashMap<>(); // порядок по добавления
-        Map<Integer,String> treeMap = new TreeMap<>();  //
-
-        test(hashMap);
-        System.out.println("########################");
-        test(linkedHashMap);
-        System.out.println("########################");
-        test(treeMap);
-
-        // aa < ab
-
-//        map.put(1, "one");
-//        map.put(2, "two");
-//        map.put(3, "three");
-//
-//        map.put(3, "hello");
-//
-//        String a = "hello";
-//
-//
-//        for (Map.Entry<Integer, String> entry: map.entrySet()) {
-//            System.out.printf("%d : %s", entry.getKey(), entry.getValue());
-//            System.out.println();
-//        }
-    }
-
-    public static void test(Map<Integer, String> map){
-        map.put(8, "eight");
-        map.put(1,"one");
-        map.put(3, "three");
-        map.put(6, "three");
-        map.put(5, "two");
-        map.put(4,"one");
-        map.put(2, "two");
-        map.put(9, "nine");
-        map.put(10, "ten");
-        map.put(7, "three");
+        Set<String> hashSet = new HashSet<>();
+        Set<String> linkedHashSet = new LinkedHashSet<>();
+        Set<String> treeSet = new TreeSet<>();
 
 
-        for (Map.Entry<Integer, String> entry: map.entrySet()) {
-            System.out.printf("%d : %s", entry.getKey(), entry.getValue());
-            System.out.println();
+        treeSet.add("Tom");
+        treeSet.add("Alex");
+        treeSet.add("John");
+        treeSet.add("Bob");
+        treeSet.add("Tim");
+
+        for(String name : treeSet){
+            System.out.println(name);
         }
+        System.out.println(treeSet.contains("Tim"));
+
+
+
+
 
     }
 
+}
 
+
+class Person{
+    private int age;
+
+
+    public Person(int age) {
+        this.age = age;
+    }
 }
